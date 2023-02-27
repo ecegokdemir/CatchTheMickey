43ec20367454728074d989df055942d3dc5a1a0e
Ece <eceegokdemir@gmail.com>
2023-02-27 23:28:21 +0300

    project finished

---
+++ app/src/main/java/com/ecegokdemir/catchthemickey/MainActivity.java
@@ -0,0 +1,138 @@
+package com.ecegokdemir.catchthemickey;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timeText,scoreText;
+    int score;
+    ImageView imageView, imageView1, imageView2, imageView3, imageView4, imageView5, imageView6,
+            imageView7, imageView8;
+
+    ImageView[] imageArray;
+
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+        score = 0;
+
+        imageView = findViewById(R.id.imageView);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+
+
+        imageArray = new ImageView[] {imageView, imageView1, imageView2, imageView3, imageView4, imageView5,
+                imageView6, imageView7, imageView8};
+
+        hideImages();
+
+        new CountDownTimer(10000,1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timeText.setText("Time: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+
+                timeText.setText("Time off!");
+                handler.removeCallbacks(runnable);
+
+                for (ImageView image: imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+                alert.setTitle("Restart?");
+                alert.setMessage("Are you sure to restart the game?");
+
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Game Over!", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                alert.show();
+
+            }
+        }.start();
+
+    }
+
+    public void increaseScore(View view){
+        score++;
+        scoreText.setText("Score: " + score);
+    }
+
+    public void hideImages() {
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image: imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+
+                Random random = new Random();
+                int i = random.nextInt(8);
+                imageArray[i].setVisibility(View.VISIBLE);
+
+                handler.postDelayed(this,500);
+            }
+        };
+
+        handler.post(runnable);
+
+
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
